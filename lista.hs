f5df1922593483630da30d8c01cc28c9682cdd37
matheus-hrm <rodriguesm673@gmail.com>
2024-08-29 14:34:59 -0300

    exercicios 1-4

--- lista.hs
+++ lista.hs
@@ -10,14 +10,16 @@ quadrado x = x * x
 media :: Int -> Int -> Int
 media x y = (x + y) `div` 2
 
-{-
-inc (quadrado 5) -> 26
-quadrado (inc 5) -> 36
-media (dobro 3) (inc 5) -> 6
--}
-
 triangulo :: Int -> Int -> Int -> Bool
 triangulo x y z | x + y > z = True
                 | x + z > y = True
                 | y + z > x = True
                 | otherwise = False
+
+triangleArea :: Float -> Float -> Float -> Float
+triangleArea x y z = sqrt (s * (s - x) * (s - y) * (s - z))
+    where s = (x + y + z) / 2
+
+metades :: [Int] -> ([Int], [Int])
+metades xs = (take x xs, drop x xs)
+                where x = length xs `div` 2
