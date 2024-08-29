93e03ff87bed348ea51b605d135d0df38e38c8d1
matheus-hrm <rodriguesm673@gmail.com>
2024-08-29 15:41:32 -0300

    exercicios 4-12

--- lista.hs
+++ lista.hs
@@ -23,3 +23,53 @@ triangleArea x y z = sqrt (s * (s - x) * (s - y) * (s - z))
 metades :: [Int] -> ([Int], [Int])
 metades xs = (take x xs, drop x xs)
                 where x = length xs `div` 2
+
+customLast :: [Int] -> Int
+customLast xs = head (take 1 y)
+                where y = reverse xs
+
+customLast2 :: [Int] -> Int
+customLast2 xs = head x
+                where x = reverse xs
+
+customInit :: [Int] -> [Int]
+customInit xs = reverse (tail x)
+                where x = reverse xs
+
+customInit2 :: [Int] -> [Int]
+customInit2 xs = take y xs
+                where y = length xs - 1
+
+binom :: Int -> Int -> Int
+binom n k = let 
+                nfact = product [1..n]
+                kfact = product [1..k]
+                l = product [1..(n-k)]                
+                in
+                nfact `div` (kfact * l )
+
+xor :: Bool -> Bool -> Bool
+xor True b = not b
+xor False b = b
+
+
+safetail :: Num a => [a] -> [a]
+safetail [] = []
+safetail xs = tail xs
+
+safetail2 :: Num a => [a] -> [a]
+safetail2 xs = if null xs then [] else tail xs
+
+safetail3 :: Num a => [a] -> [a]
+safetail3 xs | null xs = []
+                | otherwise = tail xs
+
+curta :: Num a => [a] -> Bool
+curta xs = length xs < 3
+
+
+
+
+
+
+
